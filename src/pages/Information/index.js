05e97316181686a879e799ea882b2113ed27632e
GersonRS <gersonrs@live.com>
2021-08-16 20:50:06 -0300

    first commit

---
+++ src/pages/Information/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Butao from '../../components/Butao';
+
+import { styles } from './styles';
+
+const Information = ({route}) => {
+	return (
+		<View style={styles.container}>
+			<Text>Informações: {route.params?.id}</Text>
+      {/* <Butao nome={route.params?.texto}/> */}
+		</View>
+	);
+};
+
+export default Information;
